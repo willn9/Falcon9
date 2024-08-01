c9a930f34731e27d14016c3fa5a8c11acb65c363
willn9 <wael.nawara@gmail.com>
2024-08-01 01:38:16 -0400

    Data Collection part 1
    Python, Jupyter and CSV file

---
+++ Falcon9_extracting_from_json debug.py
@@ -0,0 +1,110 @@
+import requests
+import pandas as pd
+import numpy as np
+import datetime
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_colwidth', None)
+
+def getBoosterVersion(data):
+    for x in data['rocket']:
+       if x:
+        response = requests.get("https://api.spacexdata.com/v4/rockets/"+str(x)).json()
+        BoosterVersion.append(response['name'])
+
+def getLaunchSite(data):
+    for x in data['launchpad']:
+       if x:
+         response = requests.get("https://api.spacexdata.com/v4/launchpads/"+str(x)).json()
+         Longitude.append(response['longitude'])
+         Latitude.append(response['latitude'])
+         LaunchSite.append(response['name'])
+
+def getPayloadData(data):
+    for load in data['payloads']:
+       if load:
+        response = requests.get("https://api.spacexdata.com/v4/payloads/"+load).json()
+        PayloadMass.append(response['mass_kg'])
+        Orbit.append(response['orbit'])
+
+def getCoreData(data):
+    for core in data['cores']:
+            if core['core'] != None:
+                response = requests.get("https://api.spacexdata.com/v4/cores/"+core['core']).json()
+                Block.append(response['block'])
+                ReusedCount.append(response['reuse_count'])
+                Serial.append(response['serial'])
+            else:
+                Block.append(None)
+                ReusedCount.append(None)
+                Serial.append(None)
+            Outcome.append(str(core['landing_success'])+' '+str(core['landing_type']))
+            Flights.append(core['flight'])
+            GridFins.append(core['gridfins'])
+            Reused.append(core['reused'])
+            Legs.append(core['legs'])
+            LandingPad.append(core['landpad'])
+
+static_json_url = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DS0321EN-SkillsNetwork/datasets/API_call_spacex_api.json'
+
+response = requests.get(static_json_url)
+content = response.json()
+data = pd.json_normalize(content)
+print(data.head())
+
+data = data[['rocket', 'payloads', 'launchpad', 'cores', 'flight_number', 'date_utc']]
+data = data[data['cores'].map(len)==1]
+data = data[data['payloads'].map(len)==1]
+data['cores'] = data['cores'].map(lambda x : x[0])
+data['payloads'] = data['payloads'].map(lambda x : x[0])
+data['date'] = pd.to_datetime(data['date_utc']).dt.date
+data = data[data['date'] <= datetime.date(2020, 11, 13)]
+
+BoosterVersion = []
+PayloadMass = []
+Orbit = []
+LaunchSite = []
+Outcome = []
+Flights = []
+GridFins = []
+Reused = []
+Legs = []
+LandingPad = []
+Block = []
+ReusedCount = []
+Serial = []
+Longitude = []
+Latitude = []
+
+getBoosterVersion(data)
+getLaunchSite(data)
+getPayloadData(data)
+getCoreData(data)
+
+launch_dict = {
+    'FlightNumber': list(data['flight_number']),
+    'Date': list(data['date']),
+    'BoosterVersion': BoosterVersion,
+    'PayloadMass': PayloadMass,
+    'Orbit': Orbit,
+    'LaunchSite': LaunchSite,
+    'Outcome': Outcome,
+    'Flights': Flights,
+    'GridFins': GridFins,
+    'Reused': Reused,
+    'Legs': Legs,
+    'LandingPad': LandingPad,
+    'Block': Block,
+    'ReusedCount': ReusedCount,
+    'Serial': Serial,
+    'Longitude': Longitude,
+    'Latitude': Latitude
+}
+
+# Check the lengths of all lists in launch_dict
+for key, value in launch_dict.items():
+    print(f"{key}: {len(value)}")
+
+# Create a DataFrame from the dictionary
+launch_df = pd.DataFrame(launch_dict)
+print(launch_df.head())
